ae3f009e5b4b38c1eb0a704ec04aae51af232738
dextervargasDev <dextervargas0912@gmail.com>
2025-09-15 08:02:09 +0800

    DTOs for event. event and tickettype request dtos

---
+++ src/main/java/com/dexterv/eventticket/domain/dtos/CreateTicketTypeRequest.java
@@ -0,0 +1,13 @@
+package com.dexterv.eventticket.domain.dtos;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateTicketTypeRequest {
+    private String name;
+    private Double price;
+    private String description;
+    private Integer totalAvailable;
+}
